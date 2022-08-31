7dc1ae431239786951db4e9b1374ccdb35db7893
akb3y <alisha.barraw@gmail.com>
2022-08-31 15:35:09 -0500

    added footer

--- MealPlanner/src/App.js
+++ MealPlanner/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './components/header/Header.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 function App() {
   return (
+    <div className='app'>
     <Header />
+    <Footer />
+    </div>
   );
 }
 
---
+++ MealPlanner/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <span>Brought to you by  <a href={'https://spoonacular.com/food-api'}>SpoontacularAPI</a></span>
+    </div>
+  )
+}
+
+export default Footer;
